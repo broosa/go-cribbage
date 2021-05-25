04c14eb01756ab61a335543d95c52a1edd044068
Byron Roosa <byron@Byrons-MacBook-Air.local>
2021-05-25 00:10:17 -0400

    Inital subset sums implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/broosa/go-cribbage
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(GetSubsetSums(15, 5, []uint{1, 2, 3, 3, 9}))
+}
+
+//Pass a sorted slice as set
+func GetSubsetSums(sum uint, setSize uint, set []uint) [][]uint {
+	fmt.Println("Getting subsets", sum, setSize, set)
+	sums := make([][]uint, 0)
+	for i, n := range set {
+		if n > sum {
+			continue
+		}
+
+		if n == sum {
+			sums = append(sums, []uint{n})
+			continue
+		}
+
+		otherItems := make([]uint, 0)
+		copy(otherItems, set[:i])
+		if i < len(set)-1 {
+			otherItems = append(otherItems, set[i+1:]...)
+		}
+
+		if setSize > 2 {
+			smallerSums := GetSubsetSums(sum-n, setSize-1, otherItems)
+			for _, smallSubset := range smallerSums {
+				sums = append(sums, append(smallSubset, n))
+			}
+			continue
+		}
+
+		//Find all 2-ples {a, b} such that a + b == sum
+		doubles := make([][]uint, 0)
+		for _, d := range set[:len(set)-2] {
+			if d+n == sum {
+				doubles = append(doubles, []uint{n, d})
+			}
+		}
+		fmt.Println("Doubles", sum, setSize, set, doubles)
+		sums = append(sums, doubles...)
+
+	}
+	fmt.Println("Returning", sum, setSize, set, sums)
+	return sums
+}
